01f477814ede83ff52f3d9eee06686ee8fd1715f
Nirbhay Agrawal <agrawalnirbhay@yahoo.com>
2021-12-28 12:46:48 +0530

    Added Navbar and Friend Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/about'
+  root 'home#index'
+  resources :friends
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
